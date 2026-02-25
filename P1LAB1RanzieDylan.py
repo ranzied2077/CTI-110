68486db0f58c7dbddf0862da3c9b3bf39ea4901a
ranzied2077 <ranzied2077@student.faytechcc.edu>
2026-02-25 16:52:38 -0500

    First python assignement

---
+++ P1LAB1RanzieDylan.py
@@ -0,0 +1,7 @@
+# Ranzie Dylan
+# PLAB1RanzieDylan.py
+# input and print
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Hello,",first_name,last_name +"!","Welcome to CTI-110!")
